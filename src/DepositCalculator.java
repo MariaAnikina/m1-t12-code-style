8a9a2aabefef8d3f25d83832671c27b10cf1a5d0
Mark <mlazarew29@yandex.ru>
2023-04-08 22:39:57 +0300

    style: проанализировать название переменных и кода для соотвествия Code Style

--- src/DepositCalculator.java
+++ src/DepositCalculator.java
@@ -1,7 +1,8 @@
 import java.util.Scanner;
 
 public class DepositCalculator {
-    double calculateComplexPercent(double initialAmount, double annualRate,int years) {
+    double calculateComplexPercent(double initialAmount, double annualRate,int years) {//TODO initialAmount и
+        // annualRate - отличный выбор для названия параметров
         double pay = initialAmount * Math.pow((1 + annualRate / 12), 12 * years);
         return round(pay, 2);
     }
@@ -11,12 +12,16 @@ public class DepositCalculator {
     }
 
     double round(double value, int places) {
-        double scaLe = Math.pow(10, places);
+        double scaLe = Math.pow(10, places); //TODO в переменной scaLe должны быть все буквы строчными, p.s. у меня такая же ошибка :(
         return Math.round(value * scaLe) / scaLe;
     }
 
-    void calculateContribution() {
-        int years;
+    void calculateContribution() { //TODO я в английском не native, но Contribution мне режет глаз,
+        // все-таки в банковской терминологии "вклад" - это deposit, а contribution означает нематериальный вклад.
+        //плюс сам метод содержит не только вычисление итоговой суммы на счету, но и взаимодействует с пользователем,
+        //поэтому, возможно, calculate - не самый точный глагол для описания метода.
+
+        int years; //TODO я бы рассмотрел более точное название для переменной, может быть, yearCount или depositPeriod
         int action;
 
         Scanner scanner= new Scanner(System.in);
@@ -26,8 +31,9 @@ public class DepositCalculator {
         years = scanner.nextInt( );
         System.out.println("Выберите тип вклада, 1 - вклад с обычным процентом, 2 - вклад с капитализацией:");
         action = scanner.nextInt();
-        double outDoubleVar = 0;
-        if (action ==1) {
+        double outDoubleVar = 0; //TODO нет необходимости указывать тип переменной (DoubleVar), также слово out можно
+        // трактовать по-разному, я бы заменил на слово означающие итог, например, result.
+        if (action ==1) { //TODO отсутсвует пробел после ==
             outDoubleVar = calculateSimplePercent(initialAmount, 0.06, years);
         } else if (action == 2) {
             outDoubleVar = calculateComplexPercent(initialAmount, 0.06, years);
